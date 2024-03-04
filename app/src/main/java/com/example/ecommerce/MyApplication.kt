8fb00cfe7da76699e8f3752ed086cea90f6e55a9
a7med naser <ad0112727@gmail.com>
2024-03-04 15:39:10 +0200

    add rx internet lib

---
+++ app/src/main/java/com/example/ecommerce/MyApplication.kt
@@ -0,0 +1,33 @@
+package com.example.ecommerce
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.util.Log
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+
+class MyApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        listenToNetworkConnective()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun listenToNetworkConnective() {
+        ReactiveNetwork
+            .observeInternetConnectivity()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{isConnected ->
+               Log.d(TAG,"Connected to internet : $isConnected")
+               FirebaseCrashlytics.getInstance().setCustomKey(TAG," Connected to internet : $isConnected")
+            }
+    }
+
+    companion object{
+        const val TAG = "MyApplication"
+    }
+}

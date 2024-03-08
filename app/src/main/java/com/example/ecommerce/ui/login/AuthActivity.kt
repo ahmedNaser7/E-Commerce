e25a5bae422e3c9c93d6af5351b8f455bb4a78b5
a7med naser <ad0112727@gmail.com>
2024-03-08 02:30:31 +0200

    add auth ui and view models

---
+++ app/src/main/java/com/example/ecommerce/ui/login/AuthActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ecommerce.ui.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerce.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+}
---
+++ app/src/main/java/com/example/ecommerce/ui/login/viewmodel/AuthViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.ecommerce.ui.login.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel:ViewModel() {
+}

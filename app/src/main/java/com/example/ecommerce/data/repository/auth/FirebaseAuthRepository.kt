85a38301e8cbefdc444f1c49942bf56bc607cb81
a7med naser <ad0112727@gmail.com>
2024-03-30 07:41:11 +0200

    edit login screen and firebase repo

--- app/src/main/java/com/example/ecommerce/data/repository/auth/FirebaseAuthRepository.kt
+++ app/src/main/java/com/example/ecommerce/data/repository/auth/FirebaseAuthRepository.kt
@@ -1,11 +1,11 @@
 package com.example.ecommerce.data.repository.auth
 
-import com.example.ecommerce.data.model.Resources
+import com.example.ecommerce.data.model.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface FirebaseAuthRepository {
     suspend fun loginWithEmailAndPassword(
         email: String,
         password: String
-    ): Flow<Resources<String>>
+    ): Flow<Resource<String>>
 }
